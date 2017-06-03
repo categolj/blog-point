270f44b341e572c337ff53bddca733f5c6c89498
making <makingx@gmail.com>
2017-06-04 00:29:45 +0900

    Ops

--- src/main/java/am/ik/blog/point/CloudConfig.java
+++ src/main/java/am/ik/blog/point/CloudConfig.java
@@ -3,9 +3,11 @@ package am.ik.blog.point;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
+@Configuration
 @Profile("cloud")
 public class CloudConfig extends AbstractCloudConfig {
 	@Bean
