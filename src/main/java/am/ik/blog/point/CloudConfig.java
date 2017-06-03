60639a31f886fc28c0d47009fbfb3f86fc96c02c
making <makingx@gmail.com>
2017-06-04 00:38:37 +0900

    Add DataSource

--- src/main/java/am/ik/blog/point/CloudConfig.java
+++ src/main/java/am/ik/blog/point/CloudConfig.java
@@ -1,6 +1,9 @@
 package am.ik.blog.point;
 
+import javax.sql.DataSource;
+
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +22,11 @@ public class CloudConfig extends AbstractCloudConfig {
 	@Bean
 	ConnectionFactory zipkinRabbitConnectionFactory() {
 		return connectionFactory().rabbitConnectionFactory("zipkin-rabbit");
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.tomcat")
+	DataSource dataSource() {
+		return connectionFactory().dataSource();
 	}
 }
