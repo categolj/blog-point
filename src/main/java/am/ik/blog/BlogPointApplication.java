50326c724a3b2a36a9943aa8498a975ec42a021e
making <makingx@gmail.com>
2017-06-03 19:54:26 +0900

    Add @EnableDiscoveryClient

--- src/main/java/am/ik/blog/BlogPointApplication.java
+++ src/main/java/am/ik/blog/BlogPointApplication.java
@@ -2,6 +2,7 @@ package am.ik.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableBinding(Sink.class)
 @EnableResourceServer
+@EnableDiscoveryClient
 public class BlogPointApplication {
 
 	public static void main(String[] args) {
